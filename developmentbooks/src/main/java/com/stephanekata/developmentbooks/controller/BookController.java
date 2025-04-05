ff01a4db0e76135e7c5b57033c13068969185977
jsatheesh568 <jsatheesh568@gmail.com>
2025-04-05 11:03:18 +0530

    feat: return 5 static books in /books endpoint

--- developmentbooks/src/main/java/com/stephanekata/developmentbooks/controller/BookController.java
+++ developmentbooks/src/main/java/com/stephanekata/developmentbooks/controller/BookController.java
@@ -14,6 +14,12 @@ public class BookController {
 
     @GetMapping("/books")
     public List < Book > getAllBooks(){
-        return List.of ();
+        return List.of(
+                new Book("Clean Code", "Robert Martin", 2008),
+                new Book("The Clean Coder", "Robert Martin", 2011),
+                new Book("Clean Architecture", "Robert Martin", 2017),
+                new Book("Test Driven Development by Example", "Kent Beck", 2003),
+                new Book("Working Effectively With Legacy Code", "Michael C. Feathers", 2004)
+        );
     }
 }
