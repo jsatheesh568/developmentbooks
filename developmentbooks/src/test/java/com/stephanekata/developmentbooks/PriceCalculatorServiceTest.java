b0e8c076bae2ae5caff88cfc5e947c5dbf2a06a5
jsatheesh568 <jsatheesh568@gmail.com>
2025-04-05 16:45:26 +0530

    test: fail to expecting  for multiple books total price without discount

--- developmentbooks/src/test/java/com/stephanekata/developmentbooks/PriceCalculatorServiceTest.java
+++ developmentbooks/src/test/java/com/stephanekata/developmentbooks/PriceCalculatorServiceTest.java
@@ -1,23 +1,12 @@
 package com.stephanekata.developmentbooks;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.stephanekata.developmentbooks.controller.PriceCalculatorController;
 import com.stephanekata.developmentbooks.model.Book;
 import com.stephanekata.developmentbooks.service.PriceCalculatorService;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class PriceCalculatorServiceTest {
@@ -30,5 +19,20 @@ class PriceCalculatorServiceTest {
     double result = priceCalculatorService.calculateTotalPrice(books);
 
     assertEquals(50.0, result);
+  }
+
+  @Test
+  public void testMultipleBooksReturnTotalWithoutDiscount() {
+    PriceCalculatorService priceCalculatorService = new PriceCalculatorService();
+
+    List<Book> books = List.of(
+            new Book("Clean Code", "Robert Martin", 2008),
+            new Book("Refactoring", "Martin Fowler", 1999),
+            new Book("TDD", "Kent Beck", 2002)
+    );
+
+    double result = priceCalculatorService.calculateTotalPrice(books);
+    
+    assertEquals(100.0, result);
   }
 }
