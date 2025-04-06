d1d131d7f40c3f04cc0b97bd5a06fc890a793934
jsatheesh568 <jsatheesh568@gmail.com>
2025-04-06 08:11:47 +0530

    refactor: Use constants and clean structure in FourBookDiscountStrategy

--- developmentbooks/src/main/java/com/stephanekata/developmentbooks/discount/FourBookDiscountStrategy.java
+++ developmentbooks/src/main/java/com/stephanekata/developmentbooks/discount/FourBookDiscountStrategy.java
@@ -5,13 +5,16 @@ import com.stephanekata.developmentbooks.model.Book;
 import java.util.List;
 
 public class FourBookDiscountStrategy implements DiscountStrategy {
+
+    private static final int REQUIRED_BOOK_COUNT = 4;
+    private static final double DISCOUNT_RATE = 0.20;
     @Override
     public boolean isApplicable( List < Book > books) {
-        return books.stream().map(Book::title).distinct().count() == 4;
+        return books.stream().map(Book::title).distinct().count() == REQUIRED_BOOK_COUNT;
     }
 
     @Override
     public double applyDiscount(List<Book> books, double basePrice) {
-        return basePrice * 0.20;
+        return basePrice * DISCOUNT_RATE;
     }
 }
