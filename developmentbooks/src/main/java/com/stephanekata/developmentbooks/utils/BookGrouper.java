46ce5b7b173c8e67f7a1f4d6acd84f1218c9b114
jsatheesh568 <jsatheesh568@gmail.com>
2025-04-06 09:31:31 +0530

    feat: handle group optimize for Better Discount

--- developmentbooks/src/main/java/com/stephanekata/developmentbooks/utils/BookGrouper.java
+++ developmentbooks/src/main/java/com/stephanekata/developmentbooks/utils/BookGrouper.java
@@ -14,10 +14,9 @@ public class BookGrouper {
             List<Book> group = createGroupFromRemainingBooks(titleCountMap);
             groupedBooks.add(group);
         }
-
+        optimizeGroupsForBetterDiscount(groupedBooks);
         return groupedBooks;
     }
-
     private static Map<String, Integer> countBooksByTitle(List<Book> books) {
         Map<String, Integer> titleCountMap = new HashMap<>();
 
@@ -49,4 +48,34 @@ public class BookGrouper {
             map.remove(title);
         }
     }
+
+    private static void optimizeGroupsForBetterDiscount(List<List<Book>> groupedBooks) {
+        int groupOfFiveIndex = -1;
+        int groupOfThreeIndex = -1;
+
+        for (int i = 0; i < groupedBooks.size(); i++) {
+            int size = groupedBooks.get(i).size();
+            if (size == 5 && groupOfFiveIndex == -1) {
+                groupOfFiveIndex = i;
+            } else if (size == 3 && groupOfThreeIndex == -1) {
+                groupOfThreeIndex = i;
+            }
+        }
+
+        if (groupOfFiveIndex != -1 && groupOfThreeIndex != -1) {
+            List<Book> groupOfFive = groupedBooks.get(groupOfFiveIndex);
+            List<Book> groupOfThree = groupedBooks.get(groupOfThreeIndex);
+
+            for (Book book : new ArrayList<>(groupOfFive)) {
+                boolean alreadyExists = groupOfThree.stream()
+                        .anyMatch(b -> b.title().equals(book.title()));
+                if (!alreadyExists) {
+                    groupOfFive.remove(book);
+                    groupOfThree.add(book);
+                    break;
+                }
+            }
+        }
+    }
+
 }
